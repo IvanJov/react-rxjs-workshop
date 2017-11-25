5571eeb59b152cf74c07d9679c979a72cd073b7c
Ivan Jovanovic <ivanjov96@gmail.com>
2017-11-25 11:35:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { increase, decrease, fetchUser } from './actions';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.searchUser = this.searchUser.bind(this);
+  }
+
+  searchUser(event) {
+    this.props.fetchUser(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.props.increase}>Increase</button>
+        <button onClick={this.props.decrease}>Decrease</button>
+        <p>
+          Number: {this.props.number}
+        </p>
+
+        <p>
+          <input placeholder='Username' onChange={this.searchUser} />
+          <p>
+            <img src={this.props.image} alt='Not Found' width={100} />
+          </p>
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  number: state.counter.number,
+  image: state.user.avatar_url
+});
+
+const mapDispatchToProps = {
+  increase,
+  decrease,
+  fetchUser
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
---
+++ src/epics.js
@@ -0,0 +1,36 @@
+import 'rxjs';
+import { combineEpics } from 'redux-observable';
+import { INCREASE, DECREASE, INCREASE_STORE, DECREASE_STORE, FETCH_USER } from './constants';
+import { fetchUserSuccess, fetchUserFailed } from './actions';
+import { ajax } from 'rxjs/observable/dom/ajax';
+import { Observable } from 'rxjs';
+
+export const increaseNumber = actions$ =>
+  actions$
+    .ofType(INCREASE)
+    .delay(2000)
+    .mapTo({ type: INCREASE_STORE });
+
+export const decreaseNumber = actions$ =>
+  actions$
+    .ofType(DECREASE)
+    .delay(2000)
+    .mapTo({ type: DECREASE_STORE });
+
+export const fetchUser = actions$ =>
+  actions$
+    .ofType(FETCH_USER)
+    .filter(action => action.payload.username.length < 14)
+    .mergeMap(action =>
+      ajax.getJSON(`https://api.github.com/users/${action.payload.username}`)
+        .map(user => fetchUserSuccess(user))
+        .takeUntil(actions$.ofType(FETCH_USER))
+        .catch(error => Observable.of(fetchUserFailed()))
+    )
+
+
+export default combineEpics(
+  increaseNumber,
+  decreaseNumber,
+  fetchUser
+)
